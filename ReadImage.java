cb68a8e2b5df631591a319e12ea7e5ef6bc0b8f7
RaziAbbasi <RaaziAbbasi@Gmail.com>
2016-09-23 13:21:13 -0400

    Updated ReadImage file

--- ReadImage.java
+++ ReadImage.java
@@ -9,10 +9,25 @@ public class ReadImage {
 
   public static void main(String []args){
     BufferedImage img = null;
+    int counter = 0;
     try {
-      img = ImageIO.read(new File("img.bmp"));
+      img = ImageIO.read(new File("img2.bmp"));
       if (img.getWidth() > 0){
         System.out.println("Got image!");
+        for (int xPixel = 0 ; xPixel < img.getWidth(); xPixel++){
+
+          for (int yPixel = 0; yPixel < img.getHeight(); yPixel++){
+
+            int color = img.getRGB(xPixel, yPixel);
+            if (color==Color.BLACK.getRGB()){
+
+              counter++;
+            }
+
+          }
+
+        }
+        System.out.println("Black Pixels: " + counter);
       } else {
         System.out.println("No Image!");
       }
