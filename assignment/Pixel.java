af08f49327d6bfe6c1e66cc96e309919ce7b541a
ahme5760 <aliahmed41@hotmail.com>
2016-10-29 12:10:24 -0400

    Update Pixel.java
    Thinning

--- assignment/Pixel.java
+++ assignment/Pixel.java
@@ -4,10 +4,12 @@ public class Pixel {
     
     int x, y;
     int grayscale;
+    boolean delete = false;
     
-    public Pixel(int x, int y, int grayscale){
+    public Pixel(int x, int y, int grayscale, boolean delete){
 	this.x = x;
 	this.y = y;
 	this.grayscale = grayscale;
+	this.delete = delete;
     }
 }
