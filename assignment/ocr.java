ab53f29b32551053784f524b2e33627779b288f6
ahme5760 <aliahmed41@hotmail.com>
2016-11-08 20:42:21 -0500

    Update ocr.java
    Assignment 7

--- assignment/ocr.java
+++ assignment/ocr.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 public class ocr {
 	public static boolean continueIteration = true;
+	public static ArrayList<Symbol> syms = new ArrayList<Symbol>();
     public static BufferedImage get_image( BufferedImage img, String name ){
 	
         try {
@@ -41,6 +42,7 @@ public class ocr {
 	    System.out.println("2. Convolute image");
 	    System.out.println("3. Scale image down");
 	    System.out.println("5. Thin the image");
+	    System.out.println("6. Find feature vectors of the image");
 	    System.out.println("Q to quit");
 	    
 	    // Input
@@ -80,6 +82,11 @@ public class ocr {
 		thin(img);
 		System.out.println("------------------------------------");
 		// Thin Image
+		}else if (input.equals("6")){
+		System.out.println("------------------------------------");
+		featureVector(img);
+		System.out.println("------------------------------------");
+		// Find feature vectors of the image
 		}
 	} while (!input.equals("Q"));
     }
@@ -95,7 +102,7 @@ public class ocr {
     
     public static void count_symbols( BufferedImage img ){
 	
-	ArrayList<Symbol> syms = new ArrayList<Symbol>();
+	
 	
 	int counter = 0;
 	
@@ -417,6 +424,159 @@ public class ocr {
 	} catch (IOException e) {
 	    System.out.println("Image not saved!");
 	}
+    }
+    
+    public static void featureVector( BufferedImage img ){
+    	count_symbols(img);
+    	int q1Black,q1White,q2Black,q2White,q3Black,q3White,q4Black,q4White,q5Black,q5White
+    	,q6Black,q6White,q7Black,q7White,q8Black,q8White,q9Black,q9White;
+    	
+    	q1White = q2White = q3White = q4White = q5White = q6White = q7White = q8White =q9White 
+    			= q1Black =q2Black =q3Black =q4Black =q5Black =q6Black =q7Black =q8Black =q9Black = 0;
+    	
+    	for (int i=0; i<syms.size(); i++){
+    		List<Float> featureVector = new ArrayList<Float>();
+    		int left = syms.get(i).getExtremeLeft();
+    		int right = syms.get(i).getExtremeRight();
+    		int top = syms.get(i).getExtremeTop();
+    		int bottom = syms.get(i).getExtremeBottom();
+    		
+    		double qWidth = Math.floor((right - left)/3);
+    		double qHeight = Math.floor((bottom-top)/3);
+    		int qArea = (int)qWidth*(int)qHeight;
+    		
+    		
+    		// Quandrant 1 - top left section of 9 sectioned symbol
+    		double q1x = qWidth+left;
+    		double q1y = qHeight+top;
+    		
+    		// Quandrant 2 - top middle section of 9 sectioned symbol
+    		double q2Leftx = q1x;
+    		double q2Rightx = q1x+qWidth;
+    		
+    		// Quandrant 4 - left section of 9 sectioned symbol
+    		double q4Bottomy = q1y+qHeight;
+    		
+    		
+    		for (int j = 0; j < syms.get(i).listOfPixels.size(); j++){
+    			int pixx = syms.get(i).listOfPixels.get(j).x;
+    			int pixy = syms.get(i).listOfPixels.get(j).y;
+    			//Q1
+    			if ((pixx > left && pixx < q1x) && (pixy > top && pixy < q1y)){
+    				q1Black++;
+    			}
+    			//Q2
+    			if ((pixx > q2Leftx && pixx < q2Rightx) && (pixy < q1y && pixy > top)){
+    				q2Black++;
+    			}
+    			//Q3
+    			if ((pixx > q2Rightx && pixx < right) && (pixy < q1y && pixy > top)){
+    				q3Black++;
+    			}
+    			//Q4
+    			if ((pixx > left && pixx < q1x) && (pixy < q4Bottomy && pixy > q1y)){
+    				q4Black++;
+    			}
+    			//Q5
+    			if ((pixx > q2Leftx && pixx < q2Rightx) && (pixy < q4Bottomy && pixy > q1y)){
+    				q5Black++;
+    			}
+    			//Q6
+    			if ((pixx > q2Rightx && pixx < right) && (pixy < q4Bottomy && pixy > q1y)){
+    				q6Black++;
+    			}
+    			//Q7
+    			if ((pixx > left && pixx < q2Leftx) && (pixy < bottom && pixy > q4Bottomy)){
+    				q7Black++;
+    			}
+    			//Q8
+    			if ((pixx > q2Leftx && pixx < q2Rightx) && (pixy > q4Bottomy && pixy < bottom)){
+    				q8Black++;
+    			}
+    			//Q9
+    			if ((pixx > q2Rightx && pixx < right) && (pixy > q4Bottomy && pixy < bottom)){
+    				q9Black++;
+    			}
+    		}
+    		//Q1
+    		q1White = qArea - q1Black;
+    		featureVector.add((float)q1Black/(float)q1White);
+    		//Q2
+    		q2White = qArea - q2Black;
+    		featureVector.add((float)q2Black/(float)q2White);
+    		//Q3
+    		q3White = qArea - q3Black;
+    		featureVector.add((float)q3Black/(float)q3White);
+    		//Q4
+    		q4White = qArea - q4Black;
+    		featureVector.add((float)q4Black/(float)q4White);
+    		//Q5
+    		q5White = qArea - q5Black;
+    		featureVector.add((float)q5Black/(float)q5White);
+    		//Q6
+    		q6White = qArea - q6Black;
+    		featureVector.add((float)q6Black/(float)q6White);
+    		//Q7
+    		q7White = qArea - q7Black;
+    		featureVector.add((float)q7Black/(float)q7White);
+    		//Q8
+    		q8White = qArea - q8Black;
+    		featureVector.add((float)q8Black/(float)q8White);
+    		//Q9
+    		q9White = qArea - q9Black;
+    		featureVector.add((float)q9Black/(float)q9White);
+    		
+    		System.out.println(Arrays.toString(featureVector.toArray()));
+    		
+    		qArea = q1White = q2White = q3White = q4White = q5White = q6White = q7White = q8White =q9White 
+    				= q1Black =q2Black =q3Black =q4Black =q5Black =q6Black =q7Black =q8Black =q9Black = 0;
+    		
+    		
+    		// Running this method with computer text versions of the numbers 0-9 yields the following resultant feature vectors (respectively)
+    		/*
+			[0.84236455, 0.41935483, 1.1494253, 1.125, 0.0, 1.125, 1.0, 0.4274809, 0.9893617]
+    		[0.44803694, 2.3709676, 0.0, 0.0, 1.5802469, 0.0, 0.39955357, 2.6666667, 0.39955357]
+			[0.4537445, 0.53488374, 1.244898, 0.0, 0.33333334, 0.65829146, 1.4175824, 0.9879518, 0.40425533]
+			[0.37096775, 0.43157893, 1.2666667, 0.18055555, 0.63069546, 1.0795107, 0.46551725, 0.41666666, 1.4460431]
+			[0.0066313, 0.74482757, 0.7289294, 0.63225806, 0.055632822, 0.7289294, 0.46525097, 0.4680851, 1.7400723]
+			[1.3076923, 0.46341464, 0.31474105, 0.5, 0.49321267, 0.9354839, 0.460177, 0.4379085, 1.3239436]
+			[0.695962, 0.44242424, 0.33457944, 1.8110236, 0.43373495, 0.96153843, 1.1, 0.42231077, 1.0816326]
+			[0.4651163, 0.4651163, 2.2535212, 0.0, 0.575, 0.23529412, 0.17258883, 0.81889766, 0.0]
+			[0.9893617, 0.36247724, 1.0549451, 0.74766356, 0.8560794, 0.7641509, 1.1871345, 0.35262206, 1.0952381]
+			[1.0635839, 0.41666666, 1.1441442, 0.90909094, 0.43951613, 1.902439, 0.36, 0.43951613, 0.71634614]
+    		 */
+    		
+    		// We can now write the numbers 0-9 by hand and see how well it matches up with the computer text versions
+    		
+    		/*
+    		[0.23411979, 0.16438356, 0.18881118, 0.24542125, 0.0, 0.21863799, 0.30518234, 0.09149278, 0.30268198]
+    		[0.17475729, 0.59911895, 0.0, 0.0, 0.54468083, 0.0, 0.12037037, 0.7794118, 0.09009009]
+			[0.09874327, 0.10669078, 0.2413793, 0.0, 0.0016366612, 0.28301886, 0.15689981, 0.40689656, 0.14179105]
+			[0.10683761, 0.1285403, 0.2853598, 0.057142857, 0.26960784, 0.39247313, 0.05498982, 0.121212125, 0.3631579]
+			[0.15677321, 0.3893967, 0.0, 0.35957065, 0.26245847, 0.1764706, 0.0, 0.022880215, 0.19496855]
+			[0.35969868, 0.11764706, 0.047895502, 0.19536424, 0.11591963, 0.19536424, 0.079222724, 0.09228442, 0.2778761]
+			[0.24951644, 0.1313485, 0.09677419, 0.31836733, 0.1393298, 0.07666667, 0.3374741, 0.09677419, 0.35146442]
+			[0.086419754, 0.086419754, 0.3702422, 0.0, 0.036649216, 0.15789473, 0.008917198, 0.26923078, 0.0]
+			[0.31341302, 0.11499436, 0.16079812, 0.13417432, 0.25826973, 0.21498771, 0.19588876, 0.1013363, 0.30647293]
+			[0.29263914, 0.09422492, 0.32596686, 0.13207547, 0.125, 0.36363637, 0.0, 0.0, 0.29496402]
+    		*/
+    		
+    		// By subtracting these two feature vectors, we may obtain a margin of error for each number
+    		/*
+    		(0.608245, 0.254971, 0.960614, 0.879579, 0., 0.906362, 0.694818, 0.335988, 0.68668)
+    		(0.27328, 1.77185, 0., 0., 1.03557, 0., 0.279183, 1.88725, 0.309463)
+    		.
+    		.
+    		.
+    		.
+    		.
+    		etc
+    		
+    		*/
+    		
+    		// Now we should be able to compare the feature vectors of hand written numbers/letters with the computer generated numbers/letters and guess which number the feature vector represents!
+    		
+    	}
     }
 
 }
