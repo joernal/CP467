0238d7edf0ffa8f27133bae8cf116d603bec991c
joernal <fung1950@mylaurier.ca>
2016-12-02 17:55:44 -0500

    updated for prewitt kernel edge detection

--- assignment/ocr.java
+++ assignment/ocr.java
@@ -1,11 +1,11 @@
 package assignment;
 
-import java.awt.*; 
-import java.awt.event.*; 
-import java.awt.image.*; 
-import java.io.*; 
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.*;
+import java.io.*;
 import java.util.*;
-import javax.imageio.*; 
+import javax.imageio.*;
 import java.util.List;
 
 
@@ -13,27 +13,27 @@ public class ocr {
 	public static boolean continueIteration = true;
 	public static ArrayList<Symbol> syms = new ArrayList<Symbol>();
     public static BufferedImage get_image( BufferedImage img, String name ){
-	
+
         try {
 	    img = ImageIO.read(new File(name));
         } catch (IOException e){
 	    System.out.println("Image not found!");
         }
-	
+
         return img;
     }
-    
+
     public static void main(String []args){
-	
+
 	Scanner reader = new Scanner(System.in);
-	
+
 	BufferedImage img = null;
 	String image = "image2.bmp";
-	
+
 	img = get_image(img, image);
-	
+
 	String input = "";
-	
+
 	do {
 	    // Menu
 	    System.out.println("--- Image Processing and Pattern Recognition ---");
@@ -41,14 +41,15 @@ public class ocr {
 	    System.out.println("1. Count Symbols and Pixels");
 	    System.out.println("2. Convolute image");
 	    System.out.println("3. Scale image down");
+			System.out.println("4. Edge Detection");
 	    System.out.println("5. Thin the image");
 	    System.out.println("6. Find feature vectors of the image");
 	    System.out.println("Q to quit");
-	    
+
 	    // Input
 	    System.out.print("Select an option: ");
 	    input = reader.next();
-	    
+
 	    // Change image
 	    if (input.equals("0")){
 		System.out.println("------------------------------------");
@@ -77,6 +78,10 @@ public class ocr {
 		double threshold = reader.nextDouble();
 		scale_down(img, inputWidth, inputHeight, threshold);
 		System.out.println("------------------------------------");
+	} else if (input.equals("4")){
+				System.out.println("------------------------------------");
+				edgeDetection(img);
+				System.out.println("------------------------------------");
 	    }else if (input.equals("5")){
 		System.out.println("------------------------------------");
 		thin(img);
@@ -90,7 +95,7 @@ public class ocr {
 		}
 	} while (!input.equals("Q"));
     }
-    
+
     public static ArrayList<Symbol> merge( ArrayList<Symbol> syms, int i, int j, Pixel pix){
 	for (int k = 0; k<syms.get(i).listOfPixels.size(); k++){
 	    syms.get(j).listOfPixels.add(syms.get(i).listOfPixels.get(k));
@@ -99,18 +104,18 @@ public class ocr {
 	syms.remove(i);
 	return syms;
     }
-    
+
     public static void count_symbols( BufferedImage img ){
-	
-	
-	
+
+
+
 	int counter = 0;
-	
+
 	int merge1index = -1;
 	int merge2index = -1;
-	
+
 	boolean value = false;
-	
+
 	if (img.getWidth() > 0){
 	    int grayscale = 0;
 	    for (int xPixel = 0; xPixel < img.getWidth(); xPixel++){
@@ -136,49 +141,49 @@ public class ocr {
 						syms.get(i).addPixel(pix);
 						break outerloop;
 					    }
-					    
+
 					}
 				    }
-				    
+
           			} else {
 				    value = false;
           			}
-				
+
 			    }
-			    
+
 			    if (value != true){
           			Symbol s = new Symbol( pix );
           			syms.add(s);
-				
+
 			    }
-			    
+
 			} else {
 			    Symbol s = new Symbol(pix);
 			    syms.add(s);
 			}
-			
-			
+
+
 		    }
-		    
+
 		}
-		
+
 	    }
 	    System.out.println("Total number of Black Pixels: " + counter);
-	    
+
 	    for (int k = 0; k < syms.size(); k++){
 		System.out.println("Number of black pixels in symbol " + k + " is " + syms.get(k).listOfPixels.size());
 	    }
-	    
+
 	} else {
 	    System.out.println("No Image!");
 	}
-	
+
     }
-    
+
     public static void scale_down( BufferedImage img, int inputWidth, int inputHeight, double threshold ){
-	
+
 	BufferedImage newImage = new BufferedImage(inputWidth,inputHeight,BufferedImage.TYPE_INT_RGB);
-	
+
 	double colScaleRatio = (double) img.getWidth()/newImage.getWidth();
 	double rowScaleRatio = (double) img.getHeight()/newImage.getHeight();
 
@@ -219,38 +224,38 @@ public class ocr {
 	    System.out.println("Image not saved!");
 	}
     }
-    
+
     public static void thin(BufferedImage img){
     	// Apply ZS algorithm
     	int padding = 1;
     	BufferedImage newImage = new BufferedImage(img.getWidth()+2*padding, img.getHeight()+2*padding, img.getType());
     	// add padding around image
     	Graphics g1 = newImage.getGraphics();
-    	
+
     	g1.setColor(Color.white);
     	g1.fillRect(0,0,img.getWidth()+2*padding,img.getHeight()+2*padding);
     	g1.drawImage(img, padding, padding, null);
     	g1.dispose();
-    	
-    	
+
+
     	int rgb;
     	List<Pixel> pixels = new ArrayList<Pixel>();
     	for (int yPixel = 0; yPixel < newImage.getHeight(); yPixel++){
-    	    
+
     	    for (int xPixel = 0 ; xPixel < newImage.getWidth(); xPixel++){
     		rgb = newImage.getRGB(xPixel, yPixel);
-    		
+
     		int r = (rgb >> 16) & 0xFF;
     		int g = (rgb >> 8) & 0xFF;
     		int b = (rgb & 0xFF);
-    		
+
     		int gray = (r + g + b) / 3; // get gray value
-    		
+
     		pixels.add(new Pixel(xPixel, yPixel, gray, false));
-    		
+
     	    }
     	}
-    	
+
 
     	while(continueIteration == true){
     		int iteration = 1;
@@ -260,16 +265,16 @@ public class ocr {
     		pixels = ZSRules(pixels, iteration, img, newImage);
     		pixels = delete(pixels, img, newImage);
     	}
-    	
+
     	for (int y = 0; y < newImage.getHeight(); y++){
-    	    for (int x = 0 ; x < newImage.getWidth(); x++){    		
+    	    for (int x = 0 ; x < newImage.getWidth(); x++){
     		newImage.setRGB(x, y, ((int) (pixels.get(newImage.getWidth()*y + x).grayscale) * 0x00010101) );
-    		
+
     	    }
     	}
-    	
-    	
-    	
+
+
+
     	try {
     	    File outputfile = new File("thinned.bmp");
     	    ImageIO.write(newImage, "bmp", outputfile);
@@ -278,31 +283,31 @@ public class ocr {
     	} catch (IOException e) {
     	    System.out.println("Image not saved!");
     	}
-    	
+
     }
-    
+
     public static List<Pixel> delete(List<Pixel> pixels, BufferedImage img, BufferedImage newImage){
 		// Delete
-    	
+
     	continueIteration = false;
-    	
+
 		for(int y = 1; y <= newImage.getHeight()-1; y++){
 			for(int x = 1; x <= newImage.getWidth()-1; x++){
 				if (pixels.get(newImage.getWidth()*y + x).delete){
 					pixels.get(newImage.getWidth()*y + x).grayscale = 255;
-					
+
 					continueIteration = true;
 					pixels.set(newImage.getWidth()*y + x, new Pixel(pixels.get(newImage.getWidth()*y + x).x, pixels.get(newImage.getWidth()*y + x).y, 255, false));
-					
+
 				}
 			}
 		}
-		
+
 		return pixels;
     }
-    
+
     public static List<Pixel> ZSRules(List<Pixel> pixels, int iteration, BufferedImage img, BufferedImage newImage){
-    	
+
 		// Iteration 1
 		for(int y = 1; y < newImage.getHeight()-1; y++){
 			for(int x = 1; x < newImage.getWidth()-1; x++){
@@ -316,7 +321,7 @@ public class ocr {
 				int p7 = pixels.get(j + x + newImage.getWidth()-1).grayscale;
 				int p8 = pixels.get(j + x - 1).grayscale;
 				int p9 = pixels.get(j + x - newImage.getWidth() - 1).grayscale;
-				
+
 				int[] grays = {p2,p3,p4,p5,p6,p7,p8,p9};
 				if(p1 < 255){
 					int blackCounter = 0;
@@ -332,7 +337,7 @@ public class ocr {
 					if (grays[7]==255 && grays[0] <255){
 						zeroOneCounter++;
 					}
-					
+
 					if (iteration == 1){
 						if((blackCounter >= 2 && blackCounter <= 6)
 								&&(p2 == 255 || p4 == 255 || p6 == 255)
@@ -357,40 +362,40 @@ public class ocr {
 		}
     	return pixels;
     }
-    
+
     public static void convolute( BufferedImage img ){
 
 	int padding = 2;
-	
+
 	List<Pixel> pixels = new ArrayList<Pixel>();
-	
+
 	BufferedImage newImage = new BufferedImage(img.getWidth()+2*padding, img.getHeight()+2*padding, img.getType());
 	// add padding around image
 	Graphics g1 = newImage.getGraphics();
-	
+
 	g1.setColor(Color.white);
 	g1.fillRect(0,0,img.getWidth()+2*padding,img.getHeight()+2*padding);
 	g1.drawImage(img, padding, padding, null);
 	g1.dispose();
-	
-	
+
+
 	int rgb;
 	double[][] pixData = new double[newImage.getWidth()][newImage.getHeight()];
 	for (int yPixel = 0; yPixel < newImage.getHeight(); yPixel++){
-	    
+
 	    for (int xPixel = 0 ; xPixel < newImage.getWidth(); xPixel++){
 		rgb = newImage.getRGB(xPixel, yPixel);
-		
+
 		int r = (rgb >> 16) & 0xFF;
 		int g = (rgb >> 8) & 0xFF;
 		int b = (rgb & 0xFF);
-		
+
 		int gray = (r + g + b) / 3; // get gray value
-		
-		pixData[xPixel][yPixel] = gray;		
+
+		pixData[xPixel][yPixel] = gray;
 	    }
 	}
-	
+
 	for (int y = 0; y < newImage.getHeight(); y++){
 	    for (int x=0; x < newImage.getWidth()-4; x++){
 		pixData[x+2][y] = 0.2*pixData[x][y] +
@@ -401,7 +406,7 @@ public class ocr {
 		newImage.setRGB(x, y, ((int) (pixData[x+2][y]) * 0x00010101) );
 	    }
 	}
-	
+
 	for (int x=0; x < newImage.getWidth(); x++){
 	    for (int y = 0; y < newImage.getHeight()-4; y++){
 		pixData[x][y+2] = 0.2*pixData[x][y] +
@@ -412,10 +417,10 @@ public class ocr {
 		newImage.setRGB(x, y, ((int) (pixData[x][y+2]) * 0x00010101) );
 	    }
 	}
-	
+
 	newImage = newImage.getSubimage(2, 2, img.getWidth(), img.getHeight());
-	
-	
+
+
 	try {
 	    File outputfile = new File("convoluted.bmp");
 	    ImageIO.write(newImage, "bmp", outputfile);
@@ -425,39 +430,103 @@ public class ocr {
 	    System.out.println("Image not saved!");
 	}
     }
-    
+
+	// EDGE DETECTION METHOD
+	public static void edgeDetection( BufferedImage img ){
+
+	int padding = 1;
+
+	List<Pixel> pixels = new ArrayList<Pixel>();
+
+	BufferedImage newImage = new BufferedImage(img.getWidth()+2*padding, img.getHeight()+2*padding, img.getType());
+	// add padding around image
+	Graphics g1 = newImage.getGraphics();
+
+	g1.setColor(Color.white);
+	g1.fillRect(0,0,img.getWidth()+2*padding,img.getHeight()+2*padding);
+	g1.drawImage(img, padding, padding, null);
+	g1.dispose();
+
+
+	int rgb;
+	double[][] pixData = new double[newImage.getWidth()][newImage.getHeight()];
+	for (int yPixel = 0; yPixel < newImage.getHeight(); yPixel++){
+
+	    for (int xPixel = 0 ; xPixel < newImage.getWidth(); xPixel++){
+		rgb = newImage.getRGB(xPixel, yPixel);
+
+		int r = (rgb >> 16) & 0xFF;
+		int g = (rgb >> 8) & 0xFF;
+		int b = (rgb & 0xFF);
+
+		int gray = (r + g + b) / 3; // get gray value
+
+		pixData[xPixel][yPixel] = gray;
+	    }
+	}
+
+	for (int y = 0; y < newImage.getHeight(); y++){
+	    for (int x=0; x < newImage.getWidth()-2; x++){
+				pixData[x][y] = (-1)*pixData[x][y] +
+		    0*pixData[x+1][y] +
+		    1*pixData[x+2][y];
+		newImage.setRGB(x, y, ((int) (pixData[x+1][y]) * 0x00010101) );
+	    }
+	}
+
+	for (int x=0; x < newImage.getWidth(); x++){
+	    for (int y = 0; y < newImage.getHeight()-2; y++){
+				pixData[x][y] = (-1)*pixData[x][y] +
+		    0*pixData[x][y+1] +
+		    1*pixData[x][y+2];
+		newImage.setRGB(x, y, ((int) (pixData[x][y+1]) * 0x00010101) );
+	    }
+	}
+	newImage = newImage.getSubimage(1, 1, img.getWidth(), img.getHeight());
+
+
+	try {
+	    File outputfile = new File("edge_detection.bmp");
+	    ImageIO.write(newImage, "bmp", outputfile);
+	    Desktop.getDesktop().open(outputfile);
+	    System.out.println("Edge detected image saved as edge_detection.bmp");
+	} catch (IOException e) {
+	    System.out.println("Image not saved!");
+	}
+    }
+
     public static void featureVector( BufferedImage img ){
     	count_symbols(img);
     	int q1Black,q1White,q2Black,q2White,q3Black,q3White,q4Black,q4White,q5Black,q5White
     	,q6Black,q6White,q7Black,q7White,q8Black,q8White,q9Black,q9White;
-    	
-    	q1White = q2White = q3White = q4White = q5White = q6White = q7White = q8White =q9White 
+
+    	q1White = q2White = q3White = q4White = q5White = q6White = q7White = q8White =q9White
     			= q1Black =q2Black =q3Black =q4Black =q5Black =q6Black =q7Black =q8Black =q9Black = 0;
-    	
+
     	for (int i=0; i<syms.size(); i++){
     		List<Float> featureVector = new ArrayList<Float>();
     		int left = syms.get(i).getExtremeLeft();
     		int right = syms.get(i).getExtremeRight();
     		int top = syms.get(i).getExtremeTop();
     		int bottom = syms.get(i).getExtremeBottom();
-    		
+
     		double qWidth = Math.floor((right - left)/3);
     		double qHeight = Math.floor((bottom-top)/3);
     		int qArea = (int)qWidth*(int)qHeight;
-    		
-    		
+
+
     		// Quandrant 1 - top left section of 9 sectioned symbol
     		double q1x = qWidth+left;
     		double q1y = qHeight+top;
-    		
+
     		// Quandrant 2 - top middle section of 9 sectioned symbol
     		double q2Leftx = q1x;
     		double q2Rightx = q1x+qWidth;
-    		
+
     		// Quandrant 4 - left section of 9 sectioned symbol
     		double q4Bottomy = q1y+qHeight;
-    		
-    		
+
+
     		for (int j = 0; j < syms.get(i).listOfPixels.size(); j++){
     			int pixx = syms.get(i).listOfPixels.get(j).x;
     			int pixy = syms.get(i).listOfPixels.get(j).y;
@@ -516,13 +585,13 @@ public class ocr {
     		featureVector.add((float)q8Black/(float)qArea);
     		//Q9
     		featureVector.add((float)q9Black/(float)qArea);
-    		
+
     		System.out.println(Arrays.toString(featureVector.toArray()));
-    		
-    		qArea = q1White = q2White = q3White = q4White = q5White = q6White = q7White = q8White =q9White 
+
+    		qArea = q1White = q2White = q3White = q4White = q5White = q6White = q7White = q8White =q9White
     				= q1Black =q2Black =q3Black =q4Black =q5Black =q6Black =q7Black =q8Black =q9Black = 0;
-    		
-    		
+
+
     		// Running this method with computer text versions of the numbers 0-9 yields the following resultant feature vectors (respectively)
     		/*
 			[0.3094099, 0.7033493, 0.0, 0.0, 0.61244017, 0.0, 0.28548643, 0.72727275, 0.28548643]
@@ -536,9 +605,9 @@ public class ocr {
 			[0.5154062, 0.29411766, 0.53361344, 0.47619048, 0.30532214, 0.6554622, 0.2647059, 0.30532214, 0.41736695]
 			[0.45721924, 0.29545453, 0.53475934, 0.5294118, 0.0, 0.5294118, 0.5, 0.29946524, 0.4973262]
     		 */
-    		
+
     		// We can now write the numbers 0-9 by hand and see how well it matches up with the computer text versions
-    		
+
     		/*
 			[0.03809524, 0.22328043, 0.0, 0.0, 0.14497355, 0.0, 0.11957672, 0.23068783, 0.11851852]
 			[0.120089784, 0.16498317, 0.023569023, 0.0, 0.024691358, 0.112233445, 0.08417509, 0.26823795, 0.12345679]
@@ -551,11 +620,11 @@ public class ocr {
 			[0.21040975, 0.15393133, 0.13953489, 0.11738649, 0.15393133, 0.19379845, 0.0, 0.0, 0.1915836]
 			[0.30402932, 0.12820514, 0.21062271, 0.22344323, 0.0, 0.23076923, 0.22893773, 0.11904762, 0.31868133]
     		*/
-    		
+
     		// By subtracting these two feature vectors, we may obtain a margin of error for each number
 
     		// Now we should be able to compare the feature vectors of hand written numbers/letters with the computer generated numbers/letters and guess which number the feature vector represents!
-    		
+
     	}
     }
 
